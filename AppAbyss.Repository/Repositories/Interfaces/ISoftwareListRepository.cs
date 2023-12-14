c5a102c66a2dae00cbb24cc2cddd4b28c9236aa7
Z4K1RainbowDash <Z4K1@stud.uni-obuda.hu>
2023-12-14 03:22:02 +0100

    Add interface for SoftwareListRepository

---
+++ AppAbyss.Repository/Repositories/Interfaces/ISoftwareListRepository.cs
@@ -0,0 +1,11 @@
+using AppAbyss.Data;
+
+namespace AppAbyss.Repository.Repositories.Interfaces;
+
+/// <summary>
+/// Repository interface for SoftwareList entities
+/// </summary>
+public interface ISoftwareListRepository:IGenericRepository<SoftwareList>
+{
+    
+}
