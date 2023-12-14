2a9585ce0ee43974e2ce1d60b15538b174263f87
Z4K1RainbowDash <Z4K1@stud.uni-obuda.hu>
2023-12-14 03:19:42 +0100

    Add interface for AppUserRepository

---
+++ AppAbyss.Repository/Repositories/Interfaces/IAppUserRepository.cs
@@ -0,0 +1,11 @@
+using AppAbyss.Data;
+
+namespace AppAbyss.Repository.Repositories.Interfaces;
+
+/// <summary>
+/// Repository interface for AppUser entities
+/// </summary>
+public interface IAppUserRepository: IGenericRepository<AppUser>
+{
+    
+}
