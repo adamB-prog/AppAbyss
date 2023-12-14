551a9fa60b83f09bc56a1945611e6d0663c95e4b
Z4K1RainbowDash <Z4K1@stud.uni-obuda.hu>
2023-12-14 03:21:01 +0100

    Add interface for IconRepository

---
+++ AppAbyss.Repository/Repositories/Interfaces/IIconRepository.cs
@@ -0,0 +1,11 @@
+using AppAbyss.Data;
+
+namespace AppAbyss.Repository.Repositories.Interfaces;
+
+/// <summary>
+/// Repository interface for Icon entities
+/// </summary>
+public interface IIconRepository: IGenericRepository<Icon>
+{
+    
+}
