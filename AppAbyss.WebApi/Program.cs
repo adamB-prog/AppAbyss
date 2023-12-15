85affb0642db5ac8f10af93cd6b197da1572857c
Z4K1RainbowDash <Z4K1@stud.uni-obuda.hu>
2023-12-15 02:22:08 +0100

    Initialize database connection

--- AppAbyss.WebApi/Program.cs
+++ AppAbyss.WebApi/Program.cs
@@ -1,4 +1,9 @@
+using AppAbyss.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetValue<string>("ConnectionStrings:DefaultConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
 
 // Add services to the container.
 
@@ -7,6 +12,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options
+        .UseSqlServer(connectionString, x => x.MigrationsAssembly("AppAbyss.Repository"))
+        .UseLazyLoadingProxies();
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +26,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 }
 
 app.UseHttpsRedirection();
